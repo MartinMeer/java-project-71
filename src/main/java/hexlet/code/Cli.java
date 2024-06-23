747f60dbd10b7ee0099d942e7836773028cf0f23
OlegMartynov <olegmms@gmail.com>
2024-06-23 15:44:41 +0300

    feat(Cli): added Option -f & Parameters filepath1&2

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -1,4 +1,6 @@
 package hexlet.code;
+import java.util.concurrent.Callable;
+
 import static picocli.CommandLine.*;
 
 /*Usage: gendiff [-hV]
@@ -8,22 +10,19 @@ Compares two configuration files and shows a difference.
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
-public class Cli implements Runnable {
+public class Cli implements Callable {
 
-    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
-    boolean format;
+    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
+    private String format;
 
-    @Parameters(index = "0", description = "path to first file")
-    String filepath1;
-    @Parameters(index = "1", description = "path to second file")
-    String filepath2;
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private String filepath1;
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private String filepath2;
 
-    public void generate() {
-        System.out.println("Generate Differ of two files");
-    }
 
     @Override
-    public void run() {
-
+    public Object call() throws Exception {
+        return null;
     }
 }
