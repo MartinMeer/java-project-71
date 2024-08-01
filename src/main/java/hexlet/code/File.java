c78b9553059969725409552141b593c4ce86893b
martinmeerAT <olegmms@gmail.com3>
2024-08-01 18:03:36 +0300

    feat(File): add objectMapper

--- src/main/java/hexlet/code/File.java
+++ src/main/java/hexlet/code/File.java
@@ -1,19 +1,30 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Path;
 import java.util.Map;
 
 public class File<K, V> {
 
+    @Getter
     private Map<K, V> file;
+    @Setter
+    private Path path;
 
-    public Map<K, V> getFile() {
+
+
+    public Map<K, V> parseToMap(Path path) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        file = objectMapper.readValue((JsonParser) path, new TypeReference<Map<K, V>>(){});
         return file;
-    }
-
-    public Map<K, V> parseToMap(Path path) {
-        Map<K, V> parsed = Map.of();
-        return parsed;
 
     }
 }
