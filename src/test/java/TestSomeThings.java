6f867d1f3f444894e3a24809849226b0a4878361
martinmeerAT <olegmms@gmail.com3>
2024-08-06 18:28:37 +0300

    feat(): edit TestSomeThings

--- src/test/java/TestSomeThings.java
+++ src/test/java/TestSomeThings.java
@@ -24,30 +24,4 @@ public class TestSomeThings {
         assertEquals(map.size(), mapSet.size());
     }
 
-
-    private <K, V>Map<K, V> areEqualKeyValues(Map<K, V> first, Map<K, V> second) {
-        return (Map<K, V>) first.entrySet().stream()
-                .collect(Collectors.toMap(e -> e.getKey(),
-                        e -> e.getValue().equals(second.get(e.getKey()))));
-    }
-
-    public void whenCompareTwoHashMapKeyValuesUsingStreamAPI_thenSuccess() {
-        Map<String, String> asiaCapital3 = new HashMap<String, String>();
-        asiaCapital3.put("Japan", "Tokyo");
-        asiaCapital3.put("South Korea", "Seoul");
-        asiaCapital3.put("China", "Beijing");
-
-        Map<String, String> asiaCapital4 = new HashMap<String, String>();
-        asiaCapital4.put("South Korea", "Seoul");
-        asiaCapital4.put("Japan", "Osaka");
-        asiaCapital4.put("China", "Beijing");
-
-        Map<String, Boolean> result = areEqualKeyValues(asiaCapital3, asiaCapital4);
-
-        assertEquals(3, result.size());
-        assertThat(result, hasEntry("Japan", false));
-        assertThat(result, hasEntry("South Korea", true));
-        assertThat(result, hasEntry("China", true));
-    }
-
 }
